967f968a3bc16cf4bfc1203919adcc6a90a94864
Tom Doughty <thomashdoughty@gmail.com>
2019-11-09 20:19:05 +0000

    Add js doc comments

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+/**
+ * Function to add two numbers.
+ * @param {number} a - First number to add.
+ * @param {number} b - Second number to add.
+ * @returns {number} - Sum of a and b
+ */
 export default (a, b) => a + b;
