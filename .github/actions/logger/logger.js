e820ede485ce0cd1ae8b204dd4cd2d44d359e256
Tom Doughty <thomashdoughty@gmail.com>
2019-11-11 21:45:17 +0000

    Output all

--- .github/actions/logger/logger.js
+++ .github/actions/logger/logger.js
@@ -2,6 +2,8 @@ const core = require('@actions/core');
 const { release } = require(process.env.GITHUB_EVENT_PATH);
 
 module.exports = () => {
-	console.log(release);
-	core.setOutput('release', release);
-}
+	Object.entries(release).forEach(([key, value]) => {
+		console.log(key, value);
+		core.setOutput(key, value);
+	});
+};
