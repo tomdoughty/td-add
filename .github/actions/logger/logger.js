9c39ef82693eabf6cb42bf7e8effb27a836de50f
Tom Doughty <thomashdoughty@gmail.com>
2019-11-11 21:34:48 +0000

    Tidy up logger

--- .github/actions/logger/logger.js
+++ .github/actions/logger/logger.js
@@ -1,10 +1,7 @@
 const core = require('@actions/core');
-const eventPath = require('/home/runner/work/_temp/_github_workflow/event.json');
+const { release } = require(process.env.GITHUB_EVENT_PATH);
 
-async function run() {
-	console.log(JSON.stringify(process.env));
-	console.log(JSON.stringify(eventPath.release));
-	core.setOutput('upload_url', eventPath.release.upload_url);
+module.exports = () => {
+	console.log(release);
+	core.setOutput('release', release);
 }
-
-module.exports = run;
