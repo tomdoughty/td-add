57329666824d58c71784651413a4d821f3188359
Tom Doughty <thomashdoughty@gmail.com>
2019-11-11 19:30:11 +0000

    install all

--- .github/actions/logger/logger.js
+++ .github/actions/logger/logger.js
@@ -1,8 +1,10 @@
+const core = require('@actions/core');
 const eventPath = require('/home/runner/work/_temp/_github_workflow/event.json');
 
 async function run() {
 	console.log(JSON.stringify(process.env));
-	console.log(JSON.stringify(eventPath));
+	console.log(JSON.stringify(eventPath.release));
+	core.setOutput('upload_url', eventPath.release.upload_url);
 }
 
 module.exports = run;
