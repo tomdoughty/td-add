6410e4999b9fd5b75c3a6112c54437a1758808ed
Tom Doughty <thomashdoughty@gmail.com>
2019-11-12 10:35:15 +0000

    Handle upload myself

--- .github/actions/output-event/output-event.js
+++ .github/actions/output-event/output-event.js
@@ -1,5 +1,45 @@
 const core = require('@actions/core');
-const { release } = require(process.env.GITHUB_EVENT_PATH);
+const { GitHub } = require('@actions/github');
+const { release: { upload_url } } = require(process.env.GITHUB_EVENT_PATH);
+const fs = require('fs');
+
+async function run() {
+  try {
+    // Get authenticated GitHub client (Ocktokit): https://github.com/actions/toolkit/tree/master/packages/github#usage
+    const github = new GitHub(process.env.GITHUB_TOKEN);
+    const assetPath = core.getInput('asset_path', { required: true });
+    const assetName = core.getInput('asset_name', { required: true });
+    const assetContentType = core.getInput('asset_content_type', { required: true });
+
+    // Determine content-length for header to upload asset
+    const contentLength = filePath => fs.statSync(filePath).size;
+
+    // Setup headers for API call, see Octokit Documentation: https://octokit.github.io/rest.js/#octokit-routes-repos-upload-release-asset for more information
+    const headers = { 'content-type': assetContentType, 'content-length': contentLength(assetPath) };
+
+    // Upload a release asset
+    // API Documentation: https://developer.github.com/v3/repos/releases/#upload-a-release-asset
+    // Octokit Documentation: https://octokit.github.io/rest.js/#octokit-routes-repos-upload-release-asset
+    const uploadAssetResponse = await github.repos.uploadReleaseAsset({
+      url: upload_url,
+      headers,
+      name: assetName,
+      file: fs.readFileSync(assetPath)
+    });
+
+    // Get the browser_download_url for the uploaded release asset from the response
+    const {
+      data: { browser_download_url: browserDownloadUrl }
+    } = uploadAssetResponse;
+
+    // Set the output variable for use by other actions: https://github.com/actions/toolkit/tree/master/packages/core#inputsoutputs
+    core.setOutput('browser_download_url', browserDownloadUrl);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+module.exports = run;
 
 module.exports = () => {
 	Object.entries(release).forEach(([key, value]) => {
